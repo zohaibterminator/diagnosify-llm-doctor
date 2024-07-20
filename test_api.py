d3ae6556ad62402f4816c515d17664d10f2fc811
Mohammad Zohaib Saqib <117584067+zohaibterminator@users.noreply.github.com>
2024-07-21 01:19:11 +0500

    inference api added

--- test_api.py
+++ test_api.py
@@ -1,8 +1,16 @@
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI, UploadFile, Form
 from langchain_community.document_loaders import PyMuPDFLoader
-
+from langchain_ollama import ChatOllama
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 app = FastAPI()
+
+llm = ChatOllama(
+    model_name=os.getenv('OLLAMA_MODEL_NAME'),
+    tempreture=0
+)
 
 
 @app.post('/lab_report')
@@ -26,4 +34,24 @@ async def load_pdf(file: UploadFile):
 async def load_xrays(file: UploadFile):
     return {
         'data': file
+    }
+
+
+@app.post('/infer')
+async def infer_diagnosis(user_input: str = Form(...)):
+    # Define the messages for the Ollama model
+    messages = [
+        (
+            "system",
+            "You are a helpful AI doctor, who can help diagnose medical conditions and give recommendations for treating diseases. If you don't know the answer to a specific medical inquiry, advise seeking professional help.",
+        ),
+        (
+            "human", user_input
+        ),
+    ]
+
+    # Perform inference with the Ollama model
+    ai_msg = llm.invoke(messages)
+    return {
+        'diagnosis': ai_msg.content
     }
